3cb8c9e6cb91a6d9a1ebe43948c9c18a2c1bcbf1
nsellek <nick@seansellek.com>
2016-02-21 12:46:47 -0500

    Added test to make sure delete works

--- spec/features/person_view_spec.rb
+++ spec/features/person_view_spec.rb
@@ -51,5 +51,12 @@ describe 'the person view', type: :feature do
 		end
 	end
 
-	
+	it 'destroys a phone number' do
+		phone = person.phone_numbers.first
+		old_number = phone.number
+
+		first(:link, 'delete').click
+		expect(current_path).to eq(person_path(person))
+		expect(page).to_not have_content(old_number)
+	end
 end
